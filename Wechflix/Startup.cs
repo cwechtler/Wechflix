4c52a88015478ba732c2fa50ff3a53725f871bca
Craig Wechtler <30483550+cwechtler@users.noreply.github.com>
2020-07-29 16:28:13 -0500

    Add project files.

---
+++ Wechflix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wechflix.Startup))]
+namespace Wechflix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
